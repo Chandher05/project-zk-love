fb78bb5b75930096b513f07c1f9c8b8f17f2f15e
Chandher <chandher@designstring.com>
2023-10-14 10:32:35 +0530

    setting up routes

---
+++ src/configs/RouterPaths.jsx
@@ -0,0 +1,12 @@
+import App from "../pages/app/App";
+
+export const RouterPaths = [
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: "/login",
+        element: <h1>Login</h1>,
+    },
+];
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterPaths } from "./configs/RouterPaths.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter(RouterPaths);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);

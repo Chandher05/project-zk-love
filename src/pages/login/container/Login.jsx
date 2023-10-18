827a7c1c48bdafebdf0909df7edcd099d848ee4b
Shreyank <shreyank@designstring.com>
2023-10-18 11:01:59 +0530

    set height to login page

--- src/pages/login/container/Login.jsx
+++ src/pages/login/container/Login.jsx
@@ -3,9 +3,9 @@ import { getImage } from '../../../../utils';
 
 export default function Login() {
   return (
-    <section className='h-screen login-bg'>
-      <div className='container mx-auto flex justify-center items-center w-full max-w-sm h-[600px]'>
-        <div>
+    <section className='h-screen flex items-center'>
+      <div className='container mx-auto w-full h-[80%] py-24 max-w-sm login-bg'>
+        <div className='flex flex-col items-center'>
           <img
             src={getImage('logo_with_text.png')}
             alt='safe-date-logo'
